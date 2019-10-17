9cbdf0a8912b63a1b9488195ff2952e3ffa696e6
Naja Melan <najamelan@autistici.org>
2019-10-17 19:39:41 +0200

    remove unstable features, include readme in rustdoc and enable extra warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,27 @@
-#![deny(bare_trait_objects)]
-#![forbid(unsafe_code)]
+// See: https://github.com/rust-lang/rust/issues/44732#issuecomment-488766871
+//!
+#![ cfg_attr( feature = "external_doc", feature(external_doc)         ) ]
+#![ cfg_attr( feature = "external_doc", doc(include = "../README.md") ) ]
+//
+#![ doc    ( html_root_url = "https://docs.rs/thespis_derive" ) ]
+#![ deny   ( missing_docs                                     ) ]
+#![ forbid ( unsafe_code                                      ) ]
+#![ allow  ( clippy::suspicious_else_formatting               ) ]
+
+#![ warn
+(
+	missing_debug_implementations ,
+	missing_docs                  ,
+	nonstandard_style             ,
+	rust_2018_idioms              ,
+	trivial_casts                 ,
+	trivial_numeric_casts         ,
+	unused_extern_crates          ,
+	unused_qualifications         ,
+	single_use_lifetimes          ,
+	unreachable_pub               ,
+	variant_size_differences      ,
+)]
 
 extern crate proc_macro;
 
@@ -12,6 +34,8 @@ use
 };
 
 
+/// Generate a basic impl of Actor without any methods.
+//
 #[ proc_macro_derive( Actor ) ]
 //
 pub fn derive_actor( input: TokenStream ) -> TokenStream
